1e07f856e3c975bb1bb2d3181e00089d766c663a
Mohamed-Taha Samhaoui <msamhaou@e3r3p16.1337.ma>
2023-07-12 03:28:53 +0100

    find proper X scaling progress

--- src/ft_init.c
+++ src/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: msamhaou <msamhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 17:25:10 by msamhaou          #+#    #+#             */
-/*   Updated: 2023/07/11 04:17:35 by msamhaou         ###   ########.fr       */
+/*   Updated: 2023/07/12 03:27:38 by msamhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,11 +99,6 @@ void	ft_vertical_check(t_data *data)
 		b->x = (int)(player->xpos / data->map->off_map) * data->map->off_map - 0.00001 ;
 		b->y = player->ypos - ((player->xpos - b->x) * tan(ray->angle));
 	}
-	// if ((int)(ray->angle * RAD_TO_DEG) == 90 || (int)(ray->angle * RAD_TO_DEG) == 180 + 90)
-	// {
-	// 	b->x = player->point->x;
-	// 	b->y = player->point->y;
-	// }
 }
 
 t_vertex	*ft_horizontal_check(t_data *data)
@@ -125,12 +120,6 @@ t_vertex	*ft_horizontal_check(t_data *data)
 		b->y = (int)(player->ypos / data->map->off_map) * data->map->off_map + data->map->off_map + 0.00001;
 		b->x = player->xpos - (player->ypos - b->y) / tan(ray->angle);
 	}
-	// if (ray->angle == PI || ray->angle == 0)
-	// if ((int)(ray->angle * RAD_TO_DEG) == 360 || (int)(ray->angle * RAD_TO_DEG) == 180)
-	// {
-	// 	b->x = player->point->x;
-	// 	b->y = player->point->y;
-	// }
 	return (b);
 }
 
@@ -181,25 +170,21 @@ void	ft_expand_verti(t_data *data)
 	float	xo, yo;
 
 	rep = 0;
-	printf("here1\n");
 	if (((ray->angle < 2*PI) && (ray->angle > 3 * PI / 2)) \
 			|| ((ray->angle > 0) && (ray->angle < PI / 2)))
 	{
 		xo = data->map->off_map;
 		yo = xo * tan(ray->angle);
 	}
-	printf("here2\n");
 	if (((ray->angle <  3 * PI / 2) && (ray->angle > PI)) \
 			|| ((ray->angle < PI) && (ray->angle > PI / 2)))
 	{
 		xo = -data->map->off_map;
 		yo = xo * tan(ray->angle);
 	}
-	printf("here3\n");
 	if ((int)(ray->angle * RAD_TO_DEG) == 90 || (int)(ray->angle * RAD_TO_DEG) == (180 + 90) || data->ray->hit_point_v->y < 0 \
 				|| (int)(data->ray->hit_point_v->y) > data->map->ymap * data->map->off_map)
 		rep = data->map->ymap;
-	printf("here4\n");
 	while (rep < data->map->ymap)
 	{
 		x = (int)(data->ray->hit_point_v->x / data->map->off_map);
@@ -214,63 +199,86 @@ void	ft_expand_verti(t_data *data)
 	}
 }
 
-static void	ft_angle_adjust(t_data *data, float angle)
+static void	ft_angle_adjust(float *angle)
 {
-	t_ray	*ray;
-	t_player	*player;
-
-	ray = data->ray;
-	player = data->player;
-	ray->angle = player->angle - (angle * DEG_TO_RAD);
-	if (ray->angle > 2 * PI)
-		ray->angle -= 2 * PI;
-	if (ray->angle < 0)
-		ray->angle += 2 * PI;
+	if (*angle > 2 * PI)
+		*angle -= 2 * PI;
+	if (*angle < 0)
+		*angle += 2 * PI;
 }
 
-void	ft_draw_line_mid(t_data *data, t_vertex *ray, float dist, float angle)
+void	ft_draw_line_mid(t_data *data, float dist, int nray)
 {
-	float lineheight = (500 / dist);
-	t_vertex *point1, *point2;
+	t_vertex p1, p2;
+	t_ray	*ray;
+	static int i;
+	float	line;
 
-	point1 = malloc(sizeof(t_vertex));
-	point2 = malloc(sizeof(t_vertex));
+	ray = data->ray;
+	p1.x = (float)WIDTH/2 + (float)((ray->angle - data->player->angle) * (float)RAD_TO_DEG);
+	printf("%f\n", p1.x);
+	ft_put_pix(p1.x, HEIGHT/2, 0xFF0000, data);
+	// p2.x = ((ray->angle + 60) / 120) * WIDTH;
+	// line = (HEIGHT / (dist * cos(data->player->angle - data->ray->angle))) * 5;
+	// p1.y = HEIGHT/2 - line;
+	// p2.y = HEIGHT/2;
+	// ft_vect_draw(&p1, &p2, 0xFF0000, data);
+}
 
-	point1->x = (WIDTH / 2) + (angle * (WIDTH / 120));
-	point1->y = (HEIGHT / 2) - (lineheight / 2);
-	if (point1->y <= 0)
-		point1->y = 0;
-	point2->x = (WIDTH / 2) + (angle * (WIDTH / 120));
-	point2->y =  (HEIGHT / 2)  + (lineheight / 2);
-	if (point2->y >= HEIGHT)
-		point2->y = HEIGHT;
-	// printf("height : %f\n", point1->y);
-	// printf("height : %f\n", point2->y);
-	// printf("w : %f\n", point1->x);
-	// printf("w : %f\n", point2->x);
-	//line x = 0, line y = Height / 2  - dist /2;
-	// line x = 0, line y = HEGIHT / 2 + dist /2;
-	ft_vect_draw(point1, point2, 0xFF0000, data);
+void	ft_debug(t_player *player, t_ray *ray, int nray, float dist, t_data *data)
+{
+	float newray, nnray;
+
+	newray = player->angle - ray->angle;
+		ft_angle_adjust(&newray);
+		nnray = player->angle - newray;
+		ft_angle_adjust(&nnray);
+		printf("nray : %d\n", nray);
+		printf("dist : %f\n", dist);
+		if (nray != 30)
+		{
+			float maxdeg = ((float)60 * (float)DEG_TO_RAD);
+			ft_angle_adjust(&maxdeg);
+			float newnew = data->ray->angle / maxdeg;
+			ft_angle_adjust(&newnew);
+			printf("x to draw %f\n", (float)nray / (float)60 * ((float)(WIDTH)));
+			printf("dist of other %f \n", dist * cos(nnray));
+		}
+
+		printf("\n");
+		printf("player angle In Rad : %f\n", player->angle);
+		printf("ray angle In Rad :%f\n", ray->angle);
+		printf("Diff Rad befor addjust %f\n", player->angle - ray->angle);
+		printf("Diff Rad after addjust %f\n", newray);
+		printf("that might be the real Diff Rad after addjust %f\n", nnray);
+		printf("\n");
+		printf("player angle In Deg : %f\n", player->angle * RAD_TO_DEG);
+		printf("ray angle In Deg :%f\n", ray->angle * RAD_TO_DEG);
+		printf("that might be the real Diff Deg after addjust %f\n", nnray * RAD_TO_DEG);
+		printf("Diff Deg %f\n\n\n", newray *RAD_TO_DEG);
 }
 
 void	ft_draw_ray(t_data *data)
 {
-	t_vertex	horizontal;
-	t_vertex	vertical;
 	t_vertex	*nearest;
 	t_ray*	ray;
 	t_player	*player;
-
 	float	h, v, dist;
-	float	angle, min_angle, max_angle;
 
 	ray = data->ray;
 	player = data->player;
-	min_angle = -60;
-	max_angle = 60;
-	while (min_angle < max_angle)
+	ray->angle = player->angle - (30 * DEG_TO_RAD);
+	ft_angle_adjust(&ray->angle);
+	int	nray=0;
+	while (nray < 60)
 	{
-		ft_angle_adjust(data, min_angle);
+		if (nray != 30 && nray != 59 && nray != 50)
+		{
+			nray++;
+			ray->angle += DEG_TO_RAD;
+			ft_angle_adjust(&ray->angle);
+			continue;
+		}
 		ft_horizontal_check(data);
 		ft_expand_hori(data);
 		ft_vertical_check(data);
@@ -287,10 +295,15 @@ void	ft_draw_ray(t_data *data)
 			dist = h;
 			nearest = data->ray->hit_point_h;
 		}
-		if (dist)
-			ft_draw_line_mid(data , nearest, dist , min_angle);
+		// if (dist)
+		// {
+		// 	ft_draw_line_mid(data , dist, nray);
+		// }
+			// printf("");
 		ft_vect_draw(data->player->point, nearest, 0x00FF00, data);
-		min_angle+=0.1;
+		ray->angle += DEG_TO_RAD;
+		ft_angle_adjust(&ray->angle);
+		nray++;
 	}
 }
 
